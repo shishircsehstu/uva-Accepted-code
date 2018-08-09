62a5bdedb5c1121cced64375f79ffa00bac0de08
Md Saddam Hossain <shishir.cse.hstu@gmail.com>
2018-08-09 11:07:51 +0600

    Create 13070 - Palm trees in the snow.cpp

---
+++ 13070 - Palm trees in the snow.cpp
@@ -0,0 +1,79 @@
+//BISMILLAHIR RAHMANIR RAHIM
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<map>
+#include<string>
+#include<set>
+#include<cmath>
+#include<cctype>
+#include<stack>
+#include<cstdlib>
+#include<utility>
+#include<vector>
+#include<deque>
+#include<queue>
+#include<algorithm>
+
+#define sc scanf
+#define si(t) scanf("%d",&t)
+#define sl(t) scanf("%I64d",&t)
+#define sii(a,b) scanf("%d%d",&a,&b)
+
+#define P(a) printf("%d\n",a)
+#define PLN(a) printf("%I64d ",a)
+#define pf printf
+
+#define gcd(a,b) __gcd(a,b)
+#define ff first
+#define ss second
+#define pr1(a) cout<<a<<endl
+#define pr2(a,b) cout<<a<<" "<<b<<endl
+
+#define pii pair<int,int>
+#define pi acos(-1.0)
+#define PI 3.1415926535897932385
+#define Sin(a) sin((pi*a)/180)
+#define siz 1000001
+#define mem(ar) memset(ar,0,sizeof ar)
+#define one(x) __builtin_popcount(x)
+typedef long long ll;
+using namespace std;
+//int dx[]= {-1,-1,0,0,1,1};
+//int dy[]= {-1,0,-1,1,0,1};
+//int dx[]= {0,0,1,-1};/*4 side move*/
+//int dy[]= {-1,1,0,0};/*4 side move*/
+//int dx[]= {1,1,0,-1,-1,-1,0,1};/*8 side move*/
+//int dy[]= {0,1,1,1,0,-1,-1,-1};/*8 side move*/
+//int dx[]={1,1,2,2,-1,-1,-2,-2};/*night move*/
+//int dy[]={2,-2,1,-1,2,-2,1,-1};/*night move*/
+map<ll,bool>visi;
+double ar[1002];
+int main()
+{
+    int i,j,n,num,cnt,t;
+
+    double sum;
+    scanf("%d",&t);
+    while(t--)
+    {
+        cnt=0;
+        scanf("%d",&n);
+        sum=0;
+        for(i=1; i<=n; i++)
+        {
+            scanf("%lf",&ar[i]);
+            sum+=ar[i];
+        }
+        sum=sum/n;
+        for(i=1; i<=n; i++)
+        {
+            if(ar[i]>sum)
+                cnt++;
+        }
+        sum=(cnt*100)/(double)n;
+        printf("%.3lf%c\n",sum,'%');
+    }
+
+}
+
