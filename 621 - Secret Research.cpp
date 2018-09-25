24d5b2448317d5cc40efb7aab72eb729db7fb4c8
Md Saddam Hossain <shishir.cse.hstu@gmail.com>
2018-09-25 11:11:56 +0600

    Create 621 - Secret Research.cpp

---
+++ 621 - Secret Research.cpp
@@ -0,0 +1,112 @@
+#include<bits/stdc++.h>
+
+#define all(v) v.begin(),v.end()
+#define sc scanf
+#define si(t) scanf("%d",&t)
+#define sl(t) scanf("%I64d",&t)
+
+#define sii(a,b) scanf("%d%d",&a,&b)
+
+#define pt(a) printf("%d\n",a)
+#define PLN(a) printf("%I64d\n",a)
+#define pf printf
+
+#define gcd(a,b) __gcd(a,b)
+#define ff first
+#define ss second
+
+#define pb push_back
+#define pii pair<int,int>
+#define mp make_pair
+#define pi acos(-1.0)
+#define PI 3.1415926535897932385
+#define Sin(a) sin((pi*a)/180)
+#define siz 300001
+#define mem(ar) memset(ar,0,sizeof ar)
+#define one(x) __builtin_popcount(x)
+#define mod 100000009
+#define faster ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+
+using namespace std;
+
+//int dx[]= {-1,-1,0,0,1,1};
+//int dy[]= {-1,0,-1,1,0,1};
+//int dx[]= {0,0,1,-1};/*4 side move*/
+//int dy[]= {-1,1,0,0};/*4 side move*/
+//int dx[]= {1,1,0,-1,-1,-1,0,1};/*8 side move*/
+//int dy[]= {0,1,1,1,0,-1,-1,-1};/*8 side move*/
+//int dx[]={1,1,2,2,-1,-1,-2,-2};/*knight move*/
+//int dy[]={2,-2,1,-1,2,-2,1,-1};/*knight move*/
+
+//'A'=65,'Z'=90 'a'=97 'z'=122 '0'=48
+
+
+//upper bound and lower bound
+
+#define LB(a,value) (lower_bound(all(a),value)-a.begin())
+#define UB(a,value) (upper_bound(all(a),value)-a.begin())
+//S.insert(lower_bound(S.begin( ),S.end( ),x),x); //S is vector
+
+int Set(int N,int pos)
+{
+    return N=N | (1<<pos);
+}
+int reset(int N,int pos)
+{
+    return N= N & ~(1<<pos);
+}
+bool check(int N,int pos)
+{
+    return (bool)(N & (1<<pos));
+}
+/* -a%b---------
+ll  Mod(ll  a,ll  b)
+{
+    ll  c = a % b;
+    return (c < 0) ? c + b : c;
+}
+/*
+ll  power(ll num,ll p)
+{
+    int i;
+    ll sum=1;
+    for(i=1; i<=p; i++)
+        sum*=num;
+    return sum;
+}
+*/
+string st;
+int main()
+{
+    // faster
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    int  n,i,j,a,b,x,y,ck,t,r,ans,rem,cnt,lo,hi,sum,cs=1;
+    cin>>n;
+    while(n--)
+    {
+        cin>>st;
+        r=st.size();
+        if(st[r-2]=='3'&& st[r-1]=='5')
+            cout<<"-\n";
+        else if(st[0]=='9' && st[r-1]=='4' ) cout<<"*\n";
+        else if( st[0]=='1'&& st[1]=='9'&& st[2]=='0') cout<<"?\n";
+        else cout<<"+\n";
+
+    }
+
+
+
+}
+/*
+S = 1 or S = 4 or S = 78
+• negative result S = S35
+• experiment failed S = 9S4
+• experiment not completed S = 190S
+
++ for a positive result
+- for a negative result
+* for a failed experiment
+?
+*/
