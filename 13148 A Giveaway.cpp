91be961f04e3229492fe9418a42e20d0554e6f7c
Md Saddam Hossain <shishir.cse.hstu@gmail.com>
2018-09-27 11:32:26 +0600

    Create 13148 A Giveaway.cpp

---
+++ 13148 A Giveaway.cpp
@@ -0,0 +1,105 @@
+#include<bits/stdc++.h>
+
+#define all(v) v.begin(),v.end()
+#define sc scanf
+#define si(t) scanf("%d",&t)
+#define sl(t) scanf("%I64d",&t)
+
+#define sii(a,b) scanf("%d%d",&a,&b)
+
+#define pt(a) printf("%d\n",a)
+#define PLN(a) printf("%I64d\n",a)
+#define pf printf
+
+#define gcd(a,b) __gcd(a,b)
+#define ff first
+#define ss second
+
+#define pb push_back
+#define pii pair<int,int>
+#define mp make_pair
+#define pi acos(-1.0)
+#define PI 3.1415926535897932385
+#define Sin(a) sin((pi*a)/180)
+#define siz 300001
+#define mem(ar) memset(ar,0,sizeof ar)
+#define one(x) __builtin_popcount(x)
+#define mod 100000009
+#define faster ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+
+using namespace std;
+
+//int dx[]= {-1,-1,0,0,1,1};
+//int dy[]= {-1,0,-1,1,0,1};
+//int dx[]= {0,0,1,-1};/*4 side move*/
+//int dy[]= {-1,1,0,0};/*4 side move*/
+//int dx[]= {1,1,0,-1,-1,-1,0,1};/*8 side move*/
+//int dy[]= {0,1,1,1,0,-1,-1,-1};/*8 side move*/
+//int dx[]={1,1,2,2,-1,-1,-2,-2};/*knight move*/
+//int dy[]={2,-2,1,-1,2,-2,1,-1};/*knight move*/
+
+//'A'=65,'Z'=90 'a'=97 'z'=122 '0'=48
+
+
+//upper bound and lower bound
+
+#define LB(a,value) (lower_bound(all(a),value)-a.begin())
+#define UB(a,value) (upper_bound(all(a),value)-a.begin())
+//S.insert(lower_bound(S.begin( ),S.end( ),x),x); //S is vector
+
+int Set(int N,int pos)
+{
+    return N=N | (1<<pos);
+}
+int reset(int N,int pos)
+{
+    return N= N & ~(1<<pos);
+}
+bool check(int N,int pos)
+{
+    return (bool)(N & (1<<pos));
+}
+/* -a%b---------
+ll  Mod(ll  a,ll  b)
+{
+    ll  c = a % b;
+    return (c < 0) ? c + b : c;
+}
+/*
+ll  power(ll num,ll p)
+{
+    int i;
+    ll sum=1;
+    for(i=1; i<=p; i++)
+        sum*=num;
+    return sum;
+}
+*/
+map<ll,ll>mpp;
+
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    ll n,m,i,j,a,b,x,y,ck,t,r,rem,cnt,lo,hi,sum,cs=1;
+    for(i=1; i<=10000; i++)
+    {
+        x=i*i;
+        y=i*i*i;
+        mpp[x]++;
+        mpp[y]++;
+    }
+    while(scanf("%lld",&n)==1)
+    {
+        if(n==0) break;
+        if(mpp[n]==2)
+        {
+            printf("Special\n");
+        }
+        else printf("Ordinary\n");
+    }
+    return 0;
+
+
+}
